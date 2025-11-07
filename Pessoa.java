fdd23909d3a893cc1bb60107181f62587e8572b2
julialonghi <116280754+julialonghi@users.noreply.github.com>
2025-11-07 23:27:32 +0000

    add

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    public String nome;
+    public String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void agenda() {
+        System.out.print("OI")
+    }
+}
