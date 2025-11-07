776cac8331b78bf93eaffa38d0a7bb0556c50c70
julialonghi <116280754+julialonghi@users.noreply.github.com>
2025-11-07 23:41:09 +0000

    add

--- Paciente.java
+++ Paciente.java
@@ -1,7 +1,7 @@
 public class Paciente extends Pessoa {
     public String sintoma;
 
-    public Peciente (String nome, String cpf, String sintoma){
+    public Paciente (String nome, String cpf, String sintoma){
         super(nome, cpf);
         this.sintoma = sintoma;
     }
